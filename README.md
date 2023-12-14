df95a478fa79d31a78245ddc5dcd995ac6aad992
Paul Scanlon <pauliescanlon@gmail.com>
2023-12-14 16:59:48 -0600

    feat: add rive + kwesforms

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Rive + KwesForms
+
+## Notes
+
+- You might need to disable the new Astro tool bar:
+  - `npx astro preferences disable devToolbar`
+- To use `no-reload` you'll need more than the starter plan
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,27 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        custom: {
+          'ice-1': '#e7f2f7',
+          'ice-2': '#d6e2ea',
+          'ice-3': '#c1d7e0',
+          'ice-4': '#b5b3c0',
+          'ice-5': '#b7a9b6',
+          'claret-1': '#b04863',
+          'claret-2': '#a02d62',
+          'claret-3': '#802d53',
+          'claret-4': '#716273',
+          'claret-5': '#544359',
+        },
+      },
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {},
+        },
+      }),
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 };
